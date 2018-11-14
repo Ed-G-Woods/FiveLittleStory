a1eb0aefb0c4cce2585c145bfbfd815129d003dc
DESKTOP-U98JLE6\12034 <1203448951@qq.com>
2018-11-14 17:14:51 +0800

    initialize

---
+++ FiveLittleStory/Assets/Project/Code/Player/PlayerControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerControl : MonoBehaviour {
+
+    public float speed = 50;
+
+    private Rigidbody2D _rigidbody;
+
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Start () {
+		
+	}
+	void Update () {
+		
+	}
+    private void FixedUpdate()
+    {
+        Vector2 movement = Vector2.zero;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            movement.y += 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movement.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movement.x += 1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movement.x -= 1;
+        }
+
+        _rigidbody.velocity = movement * speed;
+
+
+    }
+
+
+}
